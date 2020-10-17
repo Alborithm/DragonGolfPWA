c6c027d9b2202b49e392e477785a8943e7bd518f
Gerardo Albor <gerardoalbor@MacBook-Pro-de-Marcela.local>
2020-10-17 10:17:32 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TG.Blazor.IndexedDB;
+
+namespace DragonGolf
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddIndexedDB(dbStore =>
+                {
+                dbStore.DbName = "TheFactory"; //example name
+                dbStore.Version = 1;
+
+                dbStore.Stores.Add(new StoreSchema
+                {
+                    Name = "Employees",
+                    PrimaryKey = new IndexSpec { Name = "id", KeyPath = "id", Auto = true },
+                    Indexes = new List<IndexSpec>
+                        {
+                            new IndexSpec{Name="firstName", KeyPath = "firstName", Auto=false},
+                            new IndexSpec{Name="lastName", KeyPath = "lastName", Auto=false}
+
+                        }
+                });
+                    dbStore.Stores.Add(new StoreSchema
+                    {
+                        Name = "Outbox",
+                        PrimaryKey = new IndexSpec { Auto = true }
+                    }
+                        );
+                });
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+            //builder.Services.AddSingleton<IndexedDBManager>();
+        }
+    }
+}
